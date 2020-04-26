6986a88cd2cc73369e986877c7b2e651e2581377
Sabohat <sabohatjuraeva@gmail.com>
2020-04-26 06:31:13 +0500

    Lab 11 Finished

--- app.js
+++ app.js
@@ -2,62 +2,109 @@ window.onload=function () {
     let calculator = document.querySelector('.calculator');
     let keys = calculator.querySelector('.calculator__keys');
     const display = document.querySelector('.calculator__display');
-    keys.addEventListener('click', e => {
+    const calc = {
+        displayValue: '0',
+        firstOperand: null,
+        waitingForSecondOperand: false,
+        operator: null,
+    };
+    function updateDisplay() {
+        const display = document.querySelector('.calculator__display');
+        display.value = calc.displayValue;
+    }
+
+    updateDisplay();
+
+    keys.addEventListener('click', (e) => {
+        const { target } = e;
+        const action = target.dataset.action;
+        const keyContent = target.textContent;
+        const displayedNum = display.textContent;
+
         if (e.target.matches('button')) {
-            const key = e.target;
-            const action = key.dataset.action;
-            const keyContent = key.textContent;
-            const displayedNum = display.textContent;
+
 
             if (!action) {
-                if (displayedNum === '0') {
-                    display.textContent = keyContent
-                } else {
-                    display.textContent = displayedNum + keyContent
+                function inputDigit(digit) {
+                    const {displayValue, waitingForSecondOperand} = calc;
+
+                    if (waitingForSecondOperand === true) {
+                        calc.displayValue = digit;
+                        calc.waitingForSecondOperand = false;
+                    } else {
+                        if (displayedNum === '0') {
+                            display.textContent = keyContent
+                        } else {
+                            display.textContent = displayedNum + keyContent
+                        }
+                    }
+                    console.log(calc);
                 }
+                inputDigit(target.value);
+                updateDisplay();
             }
-            if (action === 'add') {
-                if (displayedNum === '0') {
-                    display.textContent = keyContent
-                } else {
-                    display.textContent = displayedNum + keyContent
+            if (action === 'add' ||
+                action === 'subtract' ||
+                action === 'multiply' ||
+                action === 'divide') {
+                function handleOperator(nextOperator) {
+                    const { firstOperand, displayValue, operator } = calc;
+                    const inputValue = parseFloat(displayValue);
+
+                    if (operator && calc.waitingForSecondOperand)  {
+                        calc.operator = nextOperator;
+                        console.log(calc);
+                        return;
+                    }
+
+                    if (firstOperand === null) {
+                        calc.firstOperand = inputValue;
+                    } else if (operator) {
+                        const currentValue = firstOperand || 0;
+                        const result = performCalculation[operator](currentValue, inputValue);
+
+                        calc.displayValue = String(result);
+                        calc.firstOperand = result;
+                    }
+                    calc.waitingForSecondOperand = true;
+                    calc.operator = nextOperator;
+                    console.log(calc);
                 }
-            }
-            if (action === 'subtract') {
-                if (displayedNum === '0') {
-                    display.textContent = keyContent
-                } else {
-                    display.textContent = displayedNum + keyContent
-                }
-            }
-            if (action === 'multiply') {
-                if (displayedNum === '0') {
-                    display.textContent = keyContent
-                } else {
-                    display.textContent = displayedNum + keyContent
-                }
-            }
-            if (action === 'divide') {
-                if (displayedNum === '0') {
-                    display.textContent = keyContent
-                } else {
-                    display.textContent = displayedNum + keyContent
-                }
+                const performCalculation = {
+                    '/': (firstOperand, secondOperand) => firstOperand / secondOperand,
+
+                    '*': (firstOperand, secondOperand) => firstOperand * secondOperand,
+
+                    '+': (firstOperand, secondOperand) => firstOperand + secondOperand,
+
+                    '-': (firstOperand, secondOperand) => firstOperand - secondOperand,
+
+                    '=': (firstOperand, secondOperand) => secondOperand
+                };
+                handleOperator(target.value);
+                updateDisplay();
             }
             if (action === 'decimal') {
                 function inputDecimal(dot) {
-                    if (!display.textContent.includes(dot)) {
-                        display.textContent += dot;
+                    if (calc.waitingForSecondOperand === true) return;
+
+                    if (!calc.displayValue.includes(dot)) {
+                        calc.displayValue += dot;
                     }
                 }
-                inputDecimal(key.value);
+                inputDecimal(target.value);
+                updateDisplay();
             }
             if (action === 'clear') {
-                if (displayedNum === '0') {
-                    display.textContent = keyContent
-                } else {
-                    display.textContent = displayedNum + keyContent
+                function resetCalculator() {
+                    calc.displayValue = '0';
+                    calc.firstOperand = null;
+                    calc.waitingForSecondOperand = false;
+                    calc.operator = null;
+                    console.log(calc);
                 }
+                resetCalculator();
+                updateDisplay();
             }
             if (action === 'calculate') {
                 if (displayedNum === '0') {
@@ -66,7 +113,6 @@ window.onload=function () {
                     display.textContent = displayedNum + keyContent
                 }
             }
-
         }
     })
 };
